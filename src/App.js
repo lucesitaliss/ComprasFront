dea0d23ee768cfef182de9e4855f82e22ac287d9
lissette <ing.liss87@gmail.com>
2022-12-08 13:16:03 -0300

    ajuste del router a la version 5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home/index.js'
-import Cart from './components/Cart/index'
-import Admin from './components/Admin/index'
-import Navbar from './components/Navbar/Navbar'
-import NewCart from './components/NewCart/index'
-import { Provider } from 'react-redux'
-import { store } from '../src/app/store'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//import Home from './components/Home/index.js'
+import Cart from "./components/Cart/index";
+import Admin from "./components/Admin/index";
+import Navbar from "./components/Navbar/Navbar";
+import NewCart from "./components/NewCart/index";
+import { Provider } from "react-redux";
+import { store } from "../src/app/store";
 
 export default function App() {
   return (
+    // <Provider store={store}>
+    //   <BrowserRouter>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/" element={<Cart />} />
+    //       <Route path="/newcart" element={<NewCart />} />
+    //       <Route path="/admin" element={<Admin />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </Provider>
+
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Cart />} />
-          <Route path="/newcart" element={<NewCart />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
+        <Route exact path="/">
+          <Cart />
+        </Route>
+
+        <Route path="/newcart">
+          <NewCart />
+        </Route>
+
+        <Route path="/newcart">
+          <Admin />
+        </Route>
+      </Router>
     </Provider>
-  )
+  );
 }
