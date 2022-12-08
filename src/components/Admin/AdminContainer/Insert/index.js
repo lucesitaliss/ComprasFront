0ca59a708800984a0982226b3740ddde7aeb6788
lissette <ing.liss87@gmail.com>
2022-12-08 12:23:06 -0300

    lsfjsdfkflk

--- src/components/Admin/AdminContainer/Insert/index.js
+++ src/components/Admin/AdminContainer/Insert/index.js
@@ -21,7 +21,8 @@ export default function Insert({ name }) {
         if (name === "category") {
           //const result = await fetch('http://www.localhost:4000/category', {
           const result = await fetch(
-            "https://comprasbackend-production.up.railway.app/category",
+            "https:/compras-backend-production.up.railway.app/category",
+
             {
               method: "POST",
               body: JSON.stringify(input),
