8144bcedc2af5375ea626cf30d24391f8de5dbdb
lissette <ing.liss87@gmail.com>
2022-12-08 17:17:20 -0300

    correccion de ruta d ela api

--- src/components/Admin/AdminContainer/Insert/index.js
+++ src/components/Admin/AdminContainer/Insert/index.js
@@ -21,7 +21,7 @@ export default function Insert({ name }) {
         if (name === "category") {
           //const result = await fetch('http://www.localhost:4000/category', {
           const result = await fetch(
-            "https://lucesitaliss.github.io/comprasfront/category",
+            "https://compras-backend-production.up.railway.app//category",
 
             {
               method: "POST",
