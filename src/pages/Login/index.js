eac7a2378d35e7898650705529bbc758b49fcc34
lucesitaliss <ing.liss87@gmail.com>
2023-08-25 14:23:48 -0300

    handling errors and expiration in authentication

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getApiUrl } from '../../api'
 import { useSelector, useDispatch } from 'react-redux'
-import { getToken } from '../../utils/localStorage'
+import { getToken, removeToken } from '../../utils/localStorage'
 import './login.css'
 import { Redirect } from 'react-router-dom'
 import { setLocalStorageToken } from '../../features/localStoreToken/localStoreTokenSlice'
@@ -12,6 +12,10 @@ export default function Login() {
 		password: '',
 	})
 	const [redirectCart, setRedirectCart] = useState(false)
+  const [tokenExpired, setTokenExpired] =useState(false)
+  const [errorLogin, setErrorLogin] = useState(false)
+  const [errorLoginText, setErrorLoginText] = useState("")
+
 	const dispatch = useDispatch()
 	const { token } = useSelector((state) => state.localStorageToken)
 
@@ -19,10 +23,25 @@ export default function Login() {
 		dispatch(setLocalStorageToken(getToken()))
 	}, [])
 
+  //Expiration Token
+  useEffect(() => {
+   if (tokenExpired){
+    setRedirectCart(true)
+   }
+  },[tokenExpired])
+
+
 	const handlechange = (event) => {
 		const { name, value } = event.target
 		setDataLogin({ ...dataLogin, [name]: value })
 	}
+
+  const errorMesage = {
+    401: {id: 401, message:'Required user information'},
+    402: {id:402, message:'Incorrect Credentials'},
+    403: {id:403, message:'The username or password are not correct'}
+
+  }
 
 	const handleSumitLogin = async (event) => {
 		try {
@@ -33,25 +52,51 @@ export default function Login() {
 				body: JSON.stringify(dataLogin),
 				headers: { 'content-type': 'application/json' },
 			})
+
+      Object.values(errorMesage).map((error) =>{
+        if (response.status === error.id){
+          setErrorLoginText(error.message)
+          setErrorLogin(true)
+        }else {
+          setErrorLoginText('Login is not possible')
+          setErrorLogin(true)
+        }
+      })
+    
 			const token = await response.json()
 
-			if (token) {
+			
+      if (token) {
 				localStorage.setItem('token', token)
 				dispatch(setLocalStorageToken(token))
 				setRedirectCart(true)
 			}
+      
 			event.target.reset()
+      //expired Token
+      const tokenExpiration = 7200 //time in second
+      const tokenTimer = setTimeout(() => {
+        removeToken()
+        dispatch(setLocalStorageToken(null))
+        setTokenExpired(true)
+      }, tokenExpiration * 1000)
+  
+      return () => {
+        clearTimeout(tokenTimer)
+      }
 		} catch (error) {
-			console.log(error)
+			console.error(error)
+      
 		}
 	}
-
+  
 	return (
 		<form className="loginForm" onSubmit={handleSumitLogin}>
 			<input placeholder="Enter the user" name="name" onChange={handlechange} />
 			<input
 				placeholder="Enter the password"
 				name="password"
+        type="password"
 				onChange={handlechange}
 			/>
 			<input
@@ -61,6 +106,7 @@ export default function Login() {
 				disabled={token}
 			/>
 			{redirectCart ? <Redirect to="/" /> : null}
+      {errorLogin ? <div className='errorLogin'>{errorLoginText}</div> : null}
 		</form>
 	)
 }
